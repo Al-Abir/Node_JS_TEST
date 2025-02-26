ca93d715d7f596078df2ab4ba458f86d2a82608b
Al-Abir <sheikhabir196@gmail.com>
2025-02-26 12:33:14 +0600

    Express

--- Node/index.js
+++ Node/index.js
@@ -1,1 +1,17 @@
-console.log("Hello How are you");
+const http = require('http');
+const fs = require('fs')
+
+const myServer = http.createServer((req, res)=>{
+    
+     const log= `${Date.now()}: New Request Recived\n`
+     fs.appendFile("log.txt",log,(err,data)=>{
+
+         res.end("Hello form server")
+     })
+
+
+});
+
+myServer.listen(8000, ()=>{
+    console.log("server Staterd")
+})
