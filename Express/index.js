f84735f91fafbbda3383757208f443760dc0d051
Al-Abir <sheikhabir196@gmail.com>
2025-02-27 10:31:25 +0600

    Express

--- Express/index.js
+++ Express/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const users = require('./mock.json');
 const port = 3000;
 const app = express();
 
-app.get('/', (req,res)=>{
-    res.send("Hello welcome to server")
+app.get('/api/user', (req, res)=>{
+     return  res.json(users); 
 })
+// server side rendaring always user browser
 
-app.listen(port, ()=> console.log("server is runing"))
+app.get('/user', (req, res)=>{
+
+    const userS = `<ul>
+       ${users.map((user)=> `<li> ${user.first_name}</li>`).join("")}
+    </ul>`
+
+    res.send(userS)
+})
+// dynamic route;
+
+app.get('/api/user/:id',(req,res)=>{
+    const id =Number(req.params.id);
+    const user = users.find((user)=> user.id==id)
+    res.json(user);
+
+})
+app.listen(port, ()=> `Server is running ${port} `)
